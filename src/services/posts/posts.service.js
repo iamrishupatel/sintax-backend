7eee4724ec9637802896daf0e9e4db86b2c409e5
Rishu Patel <rishupatel04@gmail.com>
2021-12-14 11:04:18 +0530

    fix: remove populate from post whitelist

--- src/services/posts/posts.service.js
+++ src/services/posts/posts.service.js
@@ -9,7 +9,6 @@ module.exports = function (app) {
   const options = {
     Model: createModel(app),
     paginate: app.get("paginate"),
-    whitelist : ["$populate"]
   };
 
   // Initialize our service with any options it requires
