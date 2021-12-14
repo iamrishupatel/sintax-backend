0388f25fc04ca0d4586b57d5c34a185833c09210
Rishu Patel <rishupatel04@gmail.com>
2021-12-14 11:51:00 +0530

    generate `comments` service

---
+++ src/services/comments/comments.class.js
@@ -0,0 +1,3 @@
+const { Service } = require("feathers-mongoose");
+
+exports.Comments = class Comments extends Service {};
--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,13 @@
 const users = require("./users/users.service.js");
 const posts = require("./posts/posts.service.js");
 const cloudinary = require("./cloudinary/cloudinary.service.js");
-const communities = require('./communities/communities.service.js');
+const communities = require("./communities/communities.service.js");
+const comments = require("./comments/comments.service.js");
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(posts);
   app.configure(cloudinary);
   app.configure(communities);
+  app.configure(comments);
 };
